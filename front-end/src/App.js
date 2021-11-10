361c6081eac12641ae27f006133697ad8e6439df
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-11-10 12:13:30 +0100

    added basic paths and boilerplate for navbar, home, and product index

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import ProductIndex from './components/ProductIndex'
 
 const App = () => {
 
   return (
-    <h1> Hello World</h1>
+    // <h1> Hello World</h1>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path='/browse' component={ProductIndex} />
+        <Route exact path='/' component={Home}/>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
---
+++ front-end/src/components/ProductIndex.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductIndex = () => {
+
+  return (
+    <h1>product index</h1>
+  )
+}
+export default ProductIndex
