f105b39f1bc38416ba8497c5f30d8c90fe2d24b5
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-11-10 13:34:03 +0100

    added basic navbar and started adding menu items

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,9 +1,27 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import {
+  Container,
+  Image,
+  Menu
+} from 'semantic-ui-react'
+
 
 const Navbar = () => {
 
+
   return (
-    <h1>navbar</h1>
+    <Menu size='large' inverted>
+      <Container>
+        <Menu.Item as='a' header>
+          <Image style={{ marginRight: '10px' }} circular size='tiny' src='https://i.insider.com/5d001393b7640142011751c9?width=1000&format=jpeg&auto=webp'/>
+        </Menu.Item>
+        <Menu.Item as='a' href='/'>Home</Menu.Item>
+        <Menu.Item as='a' href='/browse'>Browse</Menu.Item>
+        <Menu.Item as='a' href='/register'>Register</Menu.Item>
+        <Menu.Item as='a'>Login</Menu.Item>
+      </Container>
+    </Menu>
   )
 }
 
