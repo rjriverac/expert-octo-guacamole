07bd73baafb4012ab70617a40d0c12ce483c6475
Gayatri R <gayatrirajgor@gmail.com>
2021-11-11 16:29:40 +0000

    added auth.js in frontend

---
+++ front-end/src/components/helpers/auth.js
@@ -0,0 +1,12 @@
+export const getTokenFromLocalStorage = () => {
+  return window.localStorage.getItem('token')
+}
+
+export const getPayload = () => {
+  const token = getTokenFromLocalStorage()
+  if (!token) return
+  const splitToken = token.split('.')
+  if (!splitToken.length < 3) return
+  const payloadString = splitToken[1]
+  return JSON.parse(atob(payloadString))
+}
