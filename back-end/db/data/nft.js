fb121a30b22912a175c657f95edc7ef8b37d64ef
Gayatri R <gayatrirajgor@gmail.com>
2021-11-11 13:04:05 +0000

    added more nfts

--- back-end/db/data/nft.js
+++ back-end/db/data/nft.js
@@ -18,5 +18,39 @@ export default [
     name: 'Crypto Heroe on Mars',
     image: 'https://lh3.googleusercontent.com/zm5apJVmz2qLkwBsUfd4ZYBuyatp3SDvWwZVQyj2waG2OKNjFbGfWkM9ohVPgF8w_G0I0oJj6rxOM4D2WqEgAl9GigAbrAhadLAL=w600',
     category: 'Art'
+  },
+  {
+    name: 'Touch Down',
+    image: 'https://lh3.googleusercontent.com/nc6vZc9-CJ-cKYNEoTiQf4YxdMRZptzqpnDBSgMYJmeexZzpu7Uu6672oxrke0yG3FMBHOL_zG5vyfZuOPmYzXqjKgQdQxQRoLT3=w600',
+    category: 'Sports'
+  },
+  {
+    name: 'Emerald Crowns',
+    image: 'https://lh3.googleusercontent.com/QLlVIOYZkYn-3P4hVA178hYjlpVs0vD2kNKD0b4_C4W71AsORSHxzIblkOSyHnTPRVG8U8qmSo42Xvd7Dw36HXgjsUP29waOOzFZhA=w600',
+    category: 'Sports'
+  },
+  {
+    name: 'Ghost Mountain',
+    image: 'https://lh3.googleusercontent.com/tKxvVgSiAksEBDOtSviikX4L1vlQfZDNeCyFSpCs2lVaKUsQPFagJhKj1xVIGzybCgWkyBBeEjnEasYLrsXXW6eRUNv8hMNZqfC9hA=w600',
+    category: 'Sports'
+  },
+  {
+    name: 'Pepalisa Rare',
+    image: 'https://lh3.googleusercontent.com/WBDA82ODRDUqz4EVlqPuTsNcoiCYHpy2_fvUZHcNYPL1GQapQJAsTrHu_MmXFp1mRly-uwCH0wOR3i7TFzWtMyeZz--JzsFUZbn29Q=w600',
+    category: 'Utility'
+  },
+  {
+    name: 'Guatemala Nation Ownership',
+    image: 'https://lh3.googleusercontent.com/5AgeO-ebaSbMzVl0qQBER9ZgCV5Rq51oeZW-NxFV5PPqnpa7CvQyHPSyhqOqWn3qzOQu9LAhs7TSwnCHNRDeV8_N_3zTElUnvhZA=w600',
+    category: 'Virtual Worlds'
+  },
+  {
+    name: 'Bulgaria Nation Ownership',
+    image: 'https://lh3.googleusercontent.com/W95HjLnEQbQ6e2RvgRj1GMj1z1BdSmGnw4EkEGubaks1fMlGr83gQb-nWROT0dSp2hQOcfwJrVpxS4YUIm8NZXdNi2hV3BGtaUXrdA=w600',
+    category: 'Virtual Worlds'
+  }, 
+  {
+    name: 'GoonCat',
+    image: 'https://lh3.googleusercontent.com/eGEwLmXvIRaJdv3FhUI5xp7heIj7N9JTStrH7aZ-1MGkq8rSTBN983acwweRFUcBlD0XjCq1T428lBkLxOzae5-5TZ-zJeVHXYdKY5I=w600'
   }
 ]
