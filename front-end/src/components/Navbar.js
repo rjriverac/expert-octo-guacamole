ddd208930a013045019554b5a7117282fd3ecaac
lukacspapp <papplukacs@hotmail.com>
2021-11-11 10:10:16 +0000

    navbar changes

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -5,7 +5,8 @@ import {
   Image,
   Menu,
   Icon,
-  Button
+  Button,
+  Item
 } from 'semantic-ui-react'
 
 
@@ -13,14 +14,14 @@ const Navbar = () => {
 
 
   return (
-    <Menu size='large' inverted>
+    <Menu color='grey' secondary inverted size='large' >
       <Container>
         <Menu.Item as='a' href='/' header>
           <Image style={{ marginRight: '10px' }} size='mini' src='https://i.imgur.com/QC1O8bP.png'/>
           <h3 style={{ marginTop: 'unset' }}>Tokenizer</h3>
         </Menu.Item>
         <Menu.Item as='a' href='/browse'>Browse</Menu.Item>
-        <Menu.Item as='a' position='right' href='/register'><Button as='a' href='/register'>Register</Button></Menu.Item>
+        <Menu.Item position='right'><Button as='a' basic inverted href='/register'>Register</Button></Menu.Item>
         <Menu.Item as='a'><Icon name='user circle' size='large' /></Menu.Item>
       </Container>
     </Menu>
