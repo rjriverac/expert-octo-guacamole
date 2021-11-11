eb1e0035db2952747ef09095b6d9289eed1a7da8
Gayatri R <gayatrirajgor@gmail.com>
2021-11-11 12:12:49 +0000

    added 2 nft data

--- back-end/db/data/nft.js
+++ back-end/db/data/nft.js
@@ -8,5 +8,15 @@ export default [
     name: 'theCat',
     image: 'https://lh3.googleusercontent.com/DPytepoM9ZEGiLbq_9uo7rk3mCfpV8E3JgX-YtE25NjmONRNUKmd4VSY-_shzcGDDuOGq0rxyDcCWVHYsfKDBHJCN_7e8b67eYMo3g=s0',
     category: 'Portrait'
+  },
+  {
+    name: 'Bored FungleHeadz',
+    image: 'https://lh3.googleusercontent.com/ljZg3igt0LiHfAsJ8tQhc5EL2TQa9u80Z13RcEJIMPl6JWOYqBNyrnHLDLAFUyngdjEdFqzc3iTls0BWJJO82cELo5scBspM1Sn2mQ=w600',
+    category: 'Art'
+  },
+  {
+    name: 'Crypto Heroe on Mars',
+    image: 'https://lh3.googleusercontent.com/zm5apJVmz2qLkwBsUfd4ZYBuyatp3SDvWwZVQyj2waG2OKNjFbGfWkM9ohVPgF8w_G0I0oJj6rxOM4D2WqEgAl9GigAbrAhadLAL=w600',
+    category: 'Art'
   }
 ]
