ecb7097ae558eef796c7954aa22b50a9a4d41e96
Gayatri R <gayatrirajgor@gmail.com>
2021-11-11 13:24:29 +0000

    added nfts

--- back-end/db/data/nft.js
+++ back-end/db/data/nft.js
@@ -35,6 +35,11 @@ export default [
     category: 'Sports'
   },
   {
+    name: 'Gen Z Takeover',
+    image: 'https://lh3.googleusercontent.com/5ejC_Q7ba3eUL1cBQqpAEgznNm6ZJ8KWGdu28xGKqkmjYSoNLWwx0-rSAvIbuSMwd6OqjRbAhV7s29QdokeQ8mM7tGJoIkoM35ziPg=w600',
+    category: 'Sports'
+  },
+  {
     name: 'Pepalisa Rare',
     image: 'https://lh3.googleusercontent.com/WBDA82ODRDUqz4EVlqPuTsNcoiCYHpy2_fvUZHcNYPL1GQapQJAsTrHu_MmXFp1mRly-uwCH0wOR3i7TFzWtMyeZz--JzsFUZbn29Q=w600',
     category: 'Utility'
@@ -52,5 +57,24 @@ export default [
   {
     name: 'GoonCat',
     image: 'https://lh3.googleusercontent.com/eGEwLmXvIRaJdv3FhUI5xp7heIj7N9JTStrH7aZ-1MGkq8rSTBN983acwweRFUcBlD0XjCq1T428lBkLxOzae5-5TZ-zJeVHXYdKY5I=w600'
+  },
+  {
+    name: 'REVV Blur',
+    image: 'https://lh3.googleusercontent.com/NBWtbqrkLIHeWDxf0of85hlDGZESwXDB1UBZzAEwR9UBOjhF42HSGLJ7J-spqiYprIA8WvnsrylZsQMLOqYW8l4xUME-0gPWiBhT0Q=w600',
+    category: 'Sports'
+  },
+  {
+    name: 'REVV Aspire',
+    image: 'https://lh3.googleusercontent.com/LA27BRv_RMC691TKff2P86XAj1DB4MDvv0u7cpUXsFk6haYZriEwqeWifi8fsHlds0d9k-_d7LTp-AcFtqe8Gw0muq7TYefZo3ozRw=w600',
+    category: 'Sports'
+  },
+  {
+    name: 'Byteosaurus',
+    image: 'https://lh3.googleusercontent.com/-6hB7nf65kD5zh4zuOKtFhvIxa6fd0wkmTHfCwPclFW67dGieHcKFvBxuaq6eakrbkeeZXp7KinCq1OTY6nu7MyKFY23IG7m2vjY=w600',
+    category: 'Art'
+  },
+  {
+    name: 'Skullx',
+    image: 'https://lh3.googleusercontent.com/dp1gLyWgaZVCxYPEickcAoGEMOcX1wU6T1pfPy-wdcIGBJx3EutFjhEhNmEt3e1iTzWmZNbc4R7-IGDZdwJADJNltpiN5TX2WQBYuw=w600'
   }
 ]
