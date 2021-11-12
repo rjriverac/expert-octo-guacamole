666b9ea4cc994c5abb47aa5f403d6291e14c6c55
Gayatri R <gayatrirajgor@gmail.com>
2021-11-12 12:30:36 +0000

    saving changes

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,10 +1,27 @@
 /* eslint-disable no-unused-vars */ 
-import React from 'react'
-import { Segment, Grid, Button, Icon, Divider, Container, Header } from 'semantic-ui-react'
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel'
+import React, { useEffect, useState } from 'react'
+import { Segment, Grid, Button, Icon, Divider, Container, Header, Card } from 'semantic-ui-react'
+import { CarouselProvider, Image, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel'
 import 'pure-react-carousel/dist/react-carousel.es.css'
+import axios from 'axios'
 
 const Home = () => {
+
+  const [nftData, setNftData] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('/api/all')
+        if (!data) throw new Error()
+        setNftData(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getData()
+  }, [])
+
   return (
     <>
       <Container>
@@ -28,14 +45,32 @@ const Home = () => {
             totalSlides={3}
           >
             <Slider>
-              <Slide index={0}>I am the first slider.</Slide>
+              {nftData.filter((_item, index) => index < 4).map(product => {
+                <Slide>
+                  <Card>
+                    <Image src={product.image}></Image>
+                    <Card.Content>
+                      <Card.Header>{product.name}</Card.Header>
+                    </Card.Content>
+                  </Card>
+                </Slide>
+              })}
+              {/* <Slide index={0}>
+                <Card>
+                  <Image src='https://lh3.googleusercontent.com/zm5apJVmz2qLkwBsUfd4ZYBuyatp3SDvWwZVQyj2waG2OKNjFbGfWkM9ohVPgF8w_G0I0oJj6rxOM4D2WqEgAl9GigAbrAhadLAL=w600' />
+                  <Card.Content>
+                    <Card.Header>I am the first slider.</Card.Header>
+                    <Card.Meta>Displaying info</Card.Meta>
+                  </Card.Content>
+
+                </Card>
+              </Slide>
               <Slide index={1}>I am the second slider.</Slide>
-              <Slide index={2}>I am the third slider.</Slide>
+              <Slide index={2}>I am the third slider.</Slide> */}
             </Slider>
             <ButtonBack>Back</ButtonBack>
             <ButtonNext>Next</ButtonNext>
           </CarouselProvider>
-        
         </Container>
 
         <Container textAlign='center'>
