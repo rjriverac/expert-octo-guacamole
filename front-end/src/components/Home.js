09e0d04fcccc696f626ee2a17437708b7ad8814b
Gayatri R <gayatrirajgor@gmail.com>
2021-11-12 11:32:21 +0000

    added basic carousel

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */ 
 import React from 'react'
-import { Segment, Grid, Image, Button, Icon, Divider, Container, Header } from 'semantic-ui-react'
+import { Segment, Grid, Button, Icon, Divider, Container, Header } from 'semantic-ui-react'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel'
+import 'pure-react-carousel/dist/react-carousel.es.css'
 
 const Home = () => {
   return (
@@ -19,11 +21,22 @@ const Home = () => {
           textAlign='center'
         />
 
-        {/* <Container textAlign='center'>
-          <Button.Group>
-
-          </Button.Group>
-        </Container> */}
+        <Container textAlign='center'>
+          <CarouselProvider 
+            naturalSlideWidth={100}
+            naturalSlideHeight={125}
+            totalSlides={3}
+          >
+            <Slider>
+              <Slide index={0}>I am the first slider.</Slide>
+              <Slide index={1}>I am the second slider.</Slide>
+              <Slide index={2}>I am the third slider.</Slide>
+            </Slider>
+            <ButtonBack>Back</ButtonBack>
+            <ButtonNext>Next</ButtonNext>
+          </CarouselProvider>
+        
+        </Container>
 
         <Container textAlign='center'>
           <Button as='a' href='/browse'>
