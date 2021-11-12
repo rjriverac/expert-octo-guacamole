a3ee052b85f59a2bf5ecc7d44b23860029da03d8
Gayatri R <gayatrirajgor@gmail.com>
2021-11-12 14:52:57 +0000

    added styling to carousel

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */ 
 import React, { useEffect, useState } from 'react'
 import { Segment, Grid, Button, Icon, Divider, Container, Header, Card } from 'semantic-ui-react'
-import { CarouselProvider, Image, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel'
+import { CarouselProvider, Image, Slider, Slide, ButtonBack, ButtonNext, Dot } from 'pure-react-carousel'
 import 'pure-react-carousel/dist/react-carousel.es.css'
 import axios from 'axios'
 
@@ -40,39 +40,38 @@ const Home = () => {
 
         <Container textAlign='center'>
           <CarouselProvider 
-            naturalSlideWidth={100}
-            naturalSlideHeight={125}
+            naturalSlideWidth={1}
+            naturalSlideHeight={1.25}
             totalSlides={4}
+            style={{ width: '300px' }}
+            orientation='vertical'
+            visibleSlides={1}
           >
             <Slider>
               {nftData.filter((_item, index) => index < 4).map((product, index) => {
                 return (
-                  <Slide key={index}>
-                    <Card>
-                      <Image src={product.image}></Image>
-                      <Card.Content>
-                        <Card.Header>{product.name}</Card.Header>
-                      </Card.Content>
-                    </Card>
-                  </Slide>
+                  <>
+                    <Slide key={index}>
+                      <Card>
+                        <Image src={product.image}></Image>
+                        <Card.Content>
+                          <Card.Header>{product.name}</Card.Header>
+                        </Card.Content>
+                      </Card>
+                    </Slide>
+                    <Divider />
+                  </>
                 )
               })}
-
-              {/* <Slide index={0}>
-                <Card>
-                  <Image src='https://lh3.googleusercontent.com/zm5apJVmz2qLkwBsUfd4ZYBuyatp3SDvWwZVQyj2waG2OKNjFbGfWkM9ohVPgF8w_G0I0oJj6rxOM4D2WqEgAl9GigAbrAhadLAL=w600' />
-                  <Card.Content>
-                    <Card.Header>I am the first slider.</Card.Header>
-                    <Card.Meta>Displaying info</Card.Meta>
-                  </Card.Content>
-
-                </Card>
-              </Slide>
-              <Slide index={1}>I am the second slider.</Slide>
-              <Slide index={2}>I am the third slider.</Slide> */}
             </Slider>
+            {/* {nftData.filter((_item, index) => index < 4).map(slide => (
+              <Button as={Dot} key={slide} slide={slide} icon='circle'/>
+            ))} */}
             <ButtonBack>Back</ButtonBack>
             <ButtonNext>Next</ButtonNext>
+            <Button.Group>
+              
+            </Button.Group>
           </CarouselProvider>
         </Container>
 
