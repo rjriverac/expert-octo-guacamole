e950635b873282a21117830ecee8d9704f030f6d
Gayatri R <gayatrirajgor@gmail.com>
2021-11-13 13:25:20 +0000

    added funcs to nftNew

--- front-end/src/components/NFTNew.js
+++ front-end/src/components/NFTNew.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Container } from 'semantic-ui-react'
+import { getTokenFromLocalStorage } from './helpers/auth'
+import NftForm from './NftForm'
 
 const NftNew = () => {
 
+  const [formData, setFormData] = useState({
+    name: '',
+    image: '',
+    category: ''
+  })
+
+  const [errors, setErrors] = useState({
+    name: '',
+    image: '',
+    category: ''
+  })
+
+  const handleChange = (event) => {
+    const newFormData = { ...formData, [event.target.name]: event.target.value }
+    setFormData(newFormData)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      await axios.post('/api/all',
+        formData, {
+          headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` }
+        }
+      )
+    } catch (err) {
+      setErrors(err.response.data.errors)
+    }
+  }
+
   return (
-    <h1>Hello</h1>
+    <>
+      <Container>
+        <NftForm 
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          formData={formData}
+          errors={errors}
+        />
+      </Container>
+    </>
   )
 }
 export default NftNew
