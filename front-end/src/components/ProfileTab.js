e00b2d81ef487952c1cbc60b0365d364247f7743
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-11-14 17:27:00 +0100

    added some info to a card on user profile

--- front-end/src/components/ProfileTab.js
+++ front-end/src/components/ProfileTab.js
@@ -6,33 +6,47 @@ import ProductCard from './ProductCard'
 const ProfileTab = (user) => {
 
   const panes = [
-    { menuItem: 'Owned', render: () => (
-      <Tab.Pane>
-        <Card.Group centered>
-          {user.owned ?
-            user.owned.map((item, index) => (
-              <ProductCard
-                key={index}
-                item={item}
-              />
-            )
-            )
-            :
-            <Dimmer inverted active>
-              <Loader content='Loading' />
-            </Dimmer>
-          }
-        </Card.Group>
-      </Tab.Pane>) },
+    { 
+      menuItem: 'Owned', render: () => (
+        <Tab.Pane>
+          <Card.Group centered>
+            {user.owned ?
+              user.owned.map((item, index) => (
+                <ProductCard
+                  key={index}
+                  item={item}
+                />
+              )
+              )
+              :
+              <Dimmer inverted active>
+                <Loader content='Loading' />
+              </Dimmer>
+            }
+          </Card.Group>
+        </Tab.Pane>
+      ) 
+    },
     {
-      menuItem: 'Cart', render: () => <Tab.Pane>
-        <Card.Group>
-          <h1>cart will go here</h1>
-        </Card.Group>
-      </Tab.Pane>
+      menuItem: 'About', render: () => ( 
+        <Tab.Pane>
+          <Card>
+            <Card.Content>
+              <Card.Header>
+                {`User Id: ${user._id}`}
+              </Card.Header>
+              <Card.Meta>
+                {`${user.owned.length} NFTs owned`}
+              </Card.Meta>
+              <Card.Description>
+                {`Current Email: ${user.email}`}
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        </Tab.Pane>
+      )
     }
   ]
-
   return (
     <Grid.Column>
       <h3>{user.image}</h3>
