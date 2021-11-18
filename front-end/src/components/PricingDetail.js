f4ebb72be20f617a850fa62759e46c262ec011ca
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-11-18 13:08:01 +0100

    fixed x-axis

--- front-end/src/components/PricingDetail.js
+++ front-end/src/components/PricingDetail.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { Header, Segment } from 'semantic-ui-react'
 import { AreaChart, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Area, Tooltip } from 'recharts'
 import { format } from 'date-fns'
@@ -8,11 +8,26 @@ const PricingDetails = ({ item }) => {
 
   const dataArray = item.transactions.filter(item => item.type === 'sale').map(sale => {
     const date = new Date(sale.createdAt)
-    const formattedDate = format(date, 'dd/MM/yy hh:mm')
+    const formattedDate = format(date, 'd MMM yy H:m')
     const price = sale.price
     return { date: formattedDate, price: price }
   })
 
+
+  class CustomLabel extends PureComponent {
+    render() {
+      // eslint-disable-next-line no-unused-vars
+      const { x, y, stroke, payload } = this.props
+      return (
+        <g transform={`translate(${x},${y})`}>
+          <text x={0} y={0} dy={10} fill="#666" transform="rotate(-25)">
+            <tspan textAnchor='end' x='0'>{payload.value.split(' ').slice(0,3).join(' ')}</tspan>
+            <tspan textAnchor='end' x='0' dy='20'>{payload.value.split(' ').slice(-1).join('')}</tspan>
+          </text>
+        </g>
+      )
+    }
+  }
 
   return (
     <Segment style={{ width: '100%', height: 380 }} textAlign='center'>
@@ -22,14 +37,14 @@ const PricingDetails = ({ item }) => {
             data={dataArray}
             margin={{
               top: 10,
-              right: 10,
+              right: 15,
               left: 0,
-              bottom: 20
+              bottom: 40
             }}
           >
             <CartesianGrid strokeDasharray='3 3' />
-            <XAxis dataKey='date' />
-            <YAxis />
+            <XAxis interval={0} dataKey='date' tick={<CustomLabel />} />
+            <YAxis dataKey='price' />
             <Tooltip />
             <Area type='monotone' dataKey='price' stroke='#8884d8' fill='#8884d8' />
           </AreaChart>
